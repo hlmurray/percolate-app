5a57a9b8f00c4c57ddd26457614486e9eb521505
Hannah Murray <hlmurray91@gmail.com>
2014-07-28 17:56:15 -0400

    feat(percolate-app): add php email functionality

--- php/send.php
+++ php/send.php
@@ -1,16 +1,16 @@
 <?php 
 $errors = '';
 $myemail = 'hlmurray91@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
+if(empty($_POST['firstname'])  || 
+   empty($_POST['lastname']) || 
+   empty($_POST['email']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
-$name = $_POST['name']; 
+$firstname = $_POST['firstname']; 
+$lastname = $_POST['lastname']; 
 $email_address = $_POST['email']; 
-$message = $_POST['message']; 
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -24,7 +24,7 @@ if( empty($errors))
   $to = $myemail; 
   $email_subject = "Contact form submission: $name";
   $email_body = "You have received a new message. ".
-  " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+  " Here are the details:\n Name: $name \n Email: $email_address"; 
   
   $headers = "From: $myemail\n"; 
   $headers .= "Reply-To: $email_address";
