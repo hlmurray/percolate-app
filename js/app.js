7f4adf2aa43050a7f19c5db865ad28d6792813c7
Hannah Murray <hlmurray91@gmail.com>
2014-07-28 22:36:00 -0400

    fix(app.js): remove default values

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 app.controller('FormCtrl', FormCtrl);
  
 function FormCtrl(){
-    this.firstname = "default";
-    this.lastname = "default";
-    this.emailaddress = "default";
+    this.firstname = "";
+    this.lastname = "";
+    this.emailaddress = "";
 }
